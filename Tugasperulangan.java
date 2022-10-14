6db49c0f5f4a8e4e615127eb6f4241ca96f4b1bc
Ratih <113415841+Ratihmardianingsih@users.noreply.github.com>
2022-10-14 18:00:57 +0700

    Add files via upload
    tugas perulangan kelompok 12 "F"

---
+++ Tugasperulangan.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugasperulangan;
+
+/**
+ *
+ * @author User
+ */
+public class Tugasperulangan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          for (int a=1; a<=4; a++) {
+            for (int b=5; b>1; b--) {
+                    for (int c=5; c<=5; c++) {
+                }
+                System.out.print("*");
+            }
+                System.out.println("0");
+                }
+                System.out.println("00000");
+       
+    }
+    
+}
